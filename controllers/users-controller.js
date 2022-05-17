98166ac309541be8acb1615941944abb09fe66e0
= <=>
2022-05-18 01:22:10 +0530

    latest

---
+++ controllers/users-controller.js
@@ -0,0 +1,55 @@
+const { v4: uuidv4 } = require("uuid");
+
+const HttpError = require("../models/http-error");
+
+const DUMMY_USERS = [
+  {
+    id: "u1",
+    name: "Mohammad Ovesh",
+    email: "test@test.com",
+    password: "testers",
+  },
+];
+
+const getUsers = (req, res, next) => {
+  res.json({ users: DUMMY_USERS });
+};
+
+const login = (req, res, next) => {
+  const { email, password } = req.body;
+
+  const identifiedUser = DUMMY_USERS.find((u) => u.email === email);
+  if (!identifiedUser || identifiedUser.password !== password) {
+    throw new HttpError(
+      "Could not identify user, credentials seems to be wrong.",
+      401
+    );
+  }
+
+  res.json({ message: "Logged in" });
+};
+
+const signup = (req, res, next) => {
+  const { name, email, password } = req.body;
+
+  const hasUser = DUMMY_USERS.find((u) => u.email === email);
+
+  if (hasUser) {
+    throw new HttpError("Could not create user, email already exists.");
+  }
+
+  const createdUser = {
+    id: uuidv4(),
+    name,
+    email,
+    password,
+  };
+
+  DUMMY_USERS.push(createdUser);
+
+  res.status(201).json({ user: createdUser });
+};
+
+exports.getUsers = getUsers;
+exports.signup = signup;
+exports.login = login;
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,12 +1,12 @@
 const express = require("express");
 
-const placesControllers = require("../controllers/place-controllers");
+const placesControllers = require("../controllers/places-controllers");
 
 const router = express.Router();
 
 router.get("/:pid", placesControllers.getPlaceById);
 
-router.get("/user/:uid", placesControllers.getPlaceByUserId);
+router.get("/user/:uid", placesControllers.getPlacesByUserId);
 
 router.post("/", placesControllers.createPlace);
 
