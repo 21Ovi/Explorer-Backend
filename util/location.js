a4925f4caa9661826879452ee1f4872f25084605
= <=>
2022-05-18 09:42:01 +0530

    latest

---
+++ util/location.js
@@ -0,0 +1,31 @@
+const axios = require("axios");
+const HttpError = require("../models/http-error");
+
+const API_KEY = "AIzaSyCYwTw-DjyZGqFbZkoCSaaLVokSS7yDDz8";
+
+async function getCoordsForAddress(address) {
+  //   return {
+  //     lat: 40.7484474,
+  //     lng: -73.9871516,
+  //   };
+  const response = await axios.get(
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
+      address
+    )}&key=${API_KEY}`
+  );
+
+  const data = response.data;
+
+  if (!data || data.status === "ZERO_RESULTS") {
+    const error = new HttpError(
+      "Could not find for the specified address",
+      422
+    );
+    throw error;
+  }
+
+  const coordinates = data.results[0].geometry.location;
+  return coordinates;
+}
+
+module.exports = getCoordsForAddress;
