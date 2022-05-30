13a54338b18786d8d67d7f2f98dea5b812811863
= <=>
2022-05-30 13:56:06 +0530

    latest

---
+++ middleware/file-upload.js
@@ -0,0 +1,24 @@
+const multer = require("multer");
+const { v4: uuidv4 } = require("uuid");
+
+const MIME_TYPE_MAP = {
+  "image/png": "png",
+  "image/jpg": "jpg",
+  "image/jpeg": "jpeg",
+};
+
+const fileUpload = multer({
+  limit: 500000,
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "uploads/images");
+    },
+    filename: (req, file, cb) => {
+      const ext = MIME_TYPE_MAP[file.mimetype];
+      cb(null, uuidv4() + "." + ext);
+    },
+  }),
+  fileFilter,
+});
+
+module.exports = fileUpload;
