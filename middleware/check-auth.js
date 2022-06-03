a7fe25785b684cb15dae2fb4fc2de315c57b885a
= <=>
2022-06-03 19:26:10 +0530

    latest

---
+++ middleware/check-auth.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+const HttpError = require("../models/http-error");
+
+module.exports = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1]; // Authorization : 'Bearer TOKEN'
+    if (!token) {
+      throw new Error("Authentication Failed!");
+    }
+    const decodedToken = jwt.verify(token, "supersecret_dont_share");
+    req.userData = { userId: decodedToken.userId };
+    next();
+  } catch (err) {
+    const error = new HttpError("Authentication Failed!", 401);
+    return next(error);
+  }
+};
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -3,12 +3,15 @@ const { check } = require("express-validator");
 
 const placesControllers = require("../controllers/places-controllers");
 const fileUpload = require("../middleware/file-upload");
+const checkAuth = require("../middleware/check-auth");
 
 const router = express.Router();
 
 router.get("/:pid", placesControllers.getPlaceById);
 
 router.get("/user/:uid", placesControllers.getPlacesByUserId);
+
+router.use(checkAuth);
 
 router.post(
   "/",
