c45ad3823f26e86d6c997ae6838782bac82b6f9a
= <=>
2022-06-03 19:38:40 +0530

    latest

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -2,6 +2,9 @@ const jwt = require("jsonwebtoken");
 const HttpError = require("../models/http-error");
 
 module.exports = (req, res, next) => {
+  if (req.method === "OPTIONS") {
+    return next();
+  }
   try {
     const token = req.headers.authorization.split(" ")[1]; // Authorization : 'Bearer TOKEN'
     if (!token) {
