153b31c32e0918e5e1f5250766ba5ff5945c7f26
= <=>
2022-06-04 22:29:05 +0530

    Finished

--- middleware/file-upload.js
+++ middleware/file-upload.js
@@ -20,7 +20,7 @@ const fileUpload = multer({
   }),
   fileFilter: (req, file, cb) => {
     const isValid = !!MIME_TYPE_MAP[file.mimetype];
-    let error = isValid ? null : new Error("Invalid mime type!");
+    let error = isValid ? null : new Error("Invalid mime type!!");
     cb(error, isValid);
   },
 });
